59330973a21c7dc83ca21da121fa896691a6dbf6
armpit sucker <houda@houda.com>
2026-03-30 13:51:29 +0100

    update product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,44 +1,9 @@
 import productModel from "../models/productModel.js"
-import { v2 as cloudinary } from "cloudinary"
-
-// Configure cloudinary
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
-
-// helper function to upload to cloudinary
-const uploadToCloudinary = (fileBuffer) => {
-    return new Promise((resolve, reject) => {
-        cloudinary.uploader.upload_stream(
-            { resource_type: "image" },
-            (error, result) => {
-                if (error) reject(error)
-                else resolve(result.secure_url)
-            }
-        ).end(fileBuffer)
-    })
-}
 
 // function for add product
 const addProduct = async (req, res) => {
     try {
-        const { producttype, name, description, price, category, subCategory, sizes, bestseller } = req.body
-
-        const image1 = req.files.image1 && req.files.image1[0]
-        const image2 = req.files.image2 && req.files.image2[0]
-        const image3 = req.files.image3 && req.files.image3[0]
-        const image4 = req.files.image4 && req.files.image4[0]
-
-        const images = [image1, image2, image3, image4].filter((item) => item !== undefined)
-
-        // upload all images to cloudinary
-        const imagesUrl = await Promise.all(
-            images.map(async (item) => {
-                return await uploadToCloudinary(item.buffer)
-            })
-        )
+        const { producttype, name, description, price, category, subCategory, sizes, bestseller, imageUrls } = req.body
 
         const productData = {
             producttype,
@@ -49,7 +14,7 @@ const addProduct = async (req, res) => {
             subCategory,
             bestseller: bestseller === "true" ? true : false,
             sizes: JSON.parse(sizes),
-            image: imagesUrl,
+            image: imageUrls,
             date: Date.now()
         }
 
